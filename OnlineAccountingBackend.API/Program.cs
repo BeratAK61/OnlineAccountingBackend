22a6af05b89644e01b5db68f194571c8bc51d32e
Berat AK <brt_ak_61@hotmail.com>
2023-02-13 13:00:32 +0300

    Add project files.

---
+++ OnlineAccountingBackend.API/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi.Models;
+using OnlineAccountingBackend.Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(AssemblyReference).Assembly);
+//bu sekilde controller'lari presentation katmaninda kullanacagimizi belirttik    
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+//builder.Services.AddSwaggerGen();  authentication ile test etmek icin farkli bir kod ekledik alt satirda
+
+builder.Services.AddSwaggerGen(setup =>
+{
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        Description = "Put **_ONLY_** yourt JWT Bearer token on textbox below!",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme,
+        }
+    };
+
+    setup.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    setup.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme , Array.Empty<string>() }
+    });
+
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ OnlineAccountingBackend.Presentation/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineAccountingBackend.Presentation.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineAccountingBackend.Presentation.Controllers
+{
+    public sealed class TestController : ApiController 
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Trabzonspor");
+        }
+
+    }
+}
+
+//sealed = baska class'larin bu class'i inheritance etmemesini saglar
